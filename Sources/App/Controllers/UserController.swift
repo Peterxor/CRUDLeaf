c67ddb6896d75974dbee6524bdaa8f6a666ea8c5
Peterxor <peter55605@gmail.com>
2018-06-20 16:24:56 +0800

    add CRUD

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -5,15 +5,36 @@ final class UserController{
     func list(req: Request) throws ->Future<View>{
         return User.query(on: req).all().flatMap({ users in
             let data = ["userlist":users]
-            return try req.view().render("users", data)
+            return try req.view().render("CRUD", data)
         })
     }
     
     func createUser(req: Request) throws -> Future<Response>{
         return try req.content.decode(User.self).flatMap({ user in
             return user.save(on: req).map({ (user) in
-                return req.redirect(to: "users")
+                return req.redirect(to: "/users")
             })
         })
     }
+    func update(req: Request) throws -> Future<Response>{
+        return try req.parameters.next(User.self).flatMap({ user in
+            return try req.content.decode(UserForm.self).flatMap({ userform in
+                user.username = userform.username
+                return user.save(on: req).map({ _ in
+                    return req.redirect(to: "/users")
+                })
+            })
+        })
+    }
+    func delete(req: Request) throws -> Future<Response>{
+        return try req.parameters.next(User.self).flatMap({ user in
+            user.delete(on: req).map({ _ in
+                return req.redirect(to: "/users")
+            })
+        })
+    }
+}
+
+struct UserForm: Content{
+    var username: String
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,4 +4,6 @@ import Vapor
 public func routes(_ router: Router) throws {
     router.get("users", use: UserController().list)
     router.post("users", use: UserController().createUser)
+    router.post("users", "update", User.parameter, use: UserController().update)
+    router.post("users", User.parameter, "delete", use: UserController().delete)
 }
